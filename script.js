4a962f1ccd66638f0d547cc0e7d9119bd619aa47
simplicioJoao <simpliciojoaopaulo35@gmail.com>
2024-06-25 09:29:22 -0300

    Formulário de contato e rodapé

--- script.js
+++ script.js
@@ -15,3 +15,14 @@ function alteraTamanho(id) {
         botao.textContent = "+";
     }
 }
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const target = document.querySelector(this.getAttribute('href'));
+        target.scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
